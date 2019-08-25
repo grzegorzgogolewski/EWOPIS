03eefee1455ffb7d4df0505fb85618daa6297e31
Grzegorz Gogolewski <grzegorz.gogolewski@gmail.com>
2019-08-25 20:46:43 +0200

    Add project files.

---
+++ EWOPIS_Tools/FbOsrodek.cs
@@ -0,0 +1,103 @@
+﻿using FirebirdSql.Data.FirebirdClient;
+using System.Data;
+using System.Text;
+
+namespace EWOPIS_Tools
+{
+
+    public class FbOsrodek
+    {
+        private string _connectionString;
+
+
+        public FbOsrodek(string dataBasaFile, string userName, string userPass)
+        {
+            _connectionString = new FbConnectionStringBuilder
+            {
+                Database = dataBasaFile,
+                UserID = userName,
+                Password = userPass,
+                DataSource = "192.168.10.121"
+            }.ToString();
+
+        }
+
+        public DataTable GetOperatyTable (string rok)
+        {
+            DataTable dtOperaty = new DataTable();
+
+            using (var connection = new FbConnection(_connectionString))
+            {
+                connection.Open();
+
+                FbCommand command = new FbCommand("SELECT uid, typ, uwagi, (SELECT NUMER FROM OPERATY_NUM1(uid)) AS operat, (SELECT NUMER FROM KERG_NUM1(id_roboty)) AS kerg, c1 || '.' || c2 || '.' || c3 ||'.' || c4 AS idmat FROM operaty where c3 = " + rok + " order by uid;", connection); // B, E, N, S
+
+                FbDataAdapter da = new FbDataAdapter(command);
+
+                da.Fill(dtOperaty);
+
+                command.Dispose();
+                connection.Close();
+            }
+
+            return dtOperaty;
+        }
+
+
+        public DataTable GetOperDokTable(string rok)
+        {
+            DataTable dtOperDok = new DataTable();
+
+            using (var connection = new FbConnection(_connectionString))
+            {
+                connection.Open();
+
+                FbCommand command = new FbCommand("SELECT uid, typ, id_ope, dokument, typ_dok, id_blob FROM operdok WHERE id_blob <> 0 AND id_ope IN (SELECT uid FROM operaty WHERE c3 = " + rok + ") order by id_ope;", connection);
+
+                FbDataAdapter da = new FbDataAdapter(command);
+
+                da.Fill(dtOperDok);
+
+                command.Dispose();
+                connection.Close();
+
+            }
+
+            return dtOperDok;
+        }
+
+        public byte[] GetFbDokTresc(int uid)
+        {
+            byte[] filedata = Encoding.ASCII.GetBytes("pusty");
+
+            using (var connection = new FbConnection(_connectionString))
+            {
+                connection.Open();
+
+                FbCommand command = new FbCommand($"SELECT tresc FROM fbdok WHERE uid = {uid};", connection);
+
+                FbDataReader reader = command.ExecuteReader();
+
+                if (reader.Read())
+                {
+                    if (reader["tresc"].ToString() != "")
+                    {
+                        filedata = (byte[]) reader["tresc"];
+                    }
+                    else
+                    {
+                        filedata = Encoding.ASCII.GetBytes("pusty");
+                    }
+                }
+
+                command.Dispose();
+                connection.Close();
+
+            }
+
+            return filedata;
+        }
+
+    }
+
+}
---
+++ ewopis/Program.cs
@@ -0,0 +1,152 @@
+﻿using System.Data;
+using EWOPIS_Tools;
+using System.IO;
+using System;
+using System.Text.RegularExpressions;
+
+namespace ewopis
+{
+    class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            FbOsrodek osrodekFdb = new FbOsrodek("C:\\baza\\OSRODEK.FDB", "sysdba", "masterkey");
+            FbOsrodek osrodekFdr = new FbOsrodek("C:\\baza\\OSRODEK.FDR", "sysdba", "masterkey");
+
+            DataTable dtOperaty = osrodekFdb.GetOperatyTable(args[0]);
+            DataTable dtOperDok = osrodekFdb.GetOperDokTable(args[0]);
+
+            int currentFile = 1;
+            int recordCount = dtOperDok.Rows.Count;
+
+            Console.WriteLine("Nastapi eksport " + recordCount + " plikow.\nWciśnij ENTER!");
+            Console.ReadKey();
+
+            foreach (DataRow rowOperDok in dtOperDok.Rows)
+            {
+
+                int idOpe = (int)rowOperDok["id_ope"];
+
+                string operatName = GetOperatName(idOpe, dtOperaty);
+
+                Console.WriteLine("Eksport pliku dla operatu:" + operatName + " [" + currentFile++ + "/" + recordCount +"]");
+
+                string dokument = rowOperDok["dokument"].ToString();
+
+                if (dokument == "")
+                {
+                    dokument = "uid_" + rowOperDok["uid"];
+                }
+
+                int typDok = 0;
+
+                if (rowOperDok["typ_dok"].ToString() !="")
+                {
+                    typDok = Convert.ToInt32(rowOperDok["typ_dok"]);
+                }
+
+                int idBlob = 0;
+
+                if (rowOperDok["id_blob"].ToString() != "")
+                {
+                    idBlob = Convert.ToInt32(rowOperDok["id_blob"]);
+                }
+
+                string ext;
+
+                switch (typDok)
+                {
+                    case 0: // 
+                        ext = ".err0";
+                        break;
+                    case 1: // 
+                        ext = ".err1";  
+                        break;
+                    case 2: // PNG
+                        ext = ".png";
+                        break;
+                    case 3: // TXT
+                        ext = ".txt";
+                        break;
+                    case 4: // JPG
+                        ext = ".jpg";
+                        break;
+                    default:
+                        ext = ".err";
+                        break;
+                }
+
+                if (!Directory.Exists("c:\\!\\" + operatName))
+                {
+                    try
+                    {
+                        Directory.CreateDirectory("c:\\!\\" + operatName);
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(e);
+                        Console.WriteLine(operatName);
+                        Console.ReadKey();
+                    }
+                    
+                }
+
+                dokument = Regex.Replace(dokument, @"[\u0000-\u001F]", string.Empty);
+                dokument = dokument.Replace(@"\", "_");
+                dokument = dokument.Replace(@"/", "_");
+
+                string nazwaPliku = "c:\\!\\" + operatName + "\\" + dokument + ext;
+
+                int numer = 0;
+
+                while (File.Exists(nazwaPliku))
+                {
+                    numer++;
+                    nazwaPliku = "c:\\!\\" + operatName + "\\" + dokument + "_" + numer + "_" + ext;
+                }
+
+                using (var fs = new FileStream(nazwaPliku, FileMode.Create))
+                {
+                    try
+                    {
+                        fs.Write(osrodekFdr.GetFbDokTresc(idBlob), 0, osrodekFdr.GetFbDokTresc(idBlob).Length);
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(e);
+                        Console.WriteLine(operatName);
+                        Console.WriteLine(dokument);
+                        Console.ReadKey();
+                    }
+                    
+                }
+
+            }
+
+        }
+
+        private static string GetOperatName(int uid, DataTable dt)
+        {
+            DataRow[] row = dt.Select("uid=" + uid);
+
+            string operat;
+
+            if (row.Length != 0)
+            {
+                string typ = row[0]["typ"].ToString();
+                string kerg = row[0]["kerg"].ToString();
+
+                 operat = row[0]["idmat"] + "__OPE_" + typ + "_" + row[0]["operat"].ToString().Replace("/", "_") + "__KRG_" + kerg.Replace("/", "_");
+            }
+            else
+            {
+                operat = "nieznany";
+            }
+
+            return operat;
+        }
+
+    }
+}
